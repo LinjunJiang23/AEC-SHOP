dbad7ea663e8cfa8070b2719b41718ed15510cff
MayJiang23 <jiangl1@appstate.edu>
2024-09-13 21:05:53 -0400

    Still working on improving auth. Implementing JWT token.

--- Web/my-react-app/src/api/Auth.js
+++ Web/my-react-app/src/api/Auth.js
@@ -1,5 +1,6 @@
 // src/api/Auth.js
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
 import { loginValidate, adminLoginValidate } from './services/authServices';
 
 const Auth = createContext();
@@ -11,25 +12,42 @@ const AuthProvider = ({ children }) => {
 	const [admin, setAdmin] = useState(null);
 	const [error, setError] = useState(null);
 	
+	useEffect(() => {
+	  const token = localStorage.getItem('token');
+	  if (token) {
+	    const decodedUser = jwt_decode(token);
+	    setLoggedIn(true);
+		setUser(decodedUser);
+	  }
+	}, []);
+	
 	// Helper Functions
 	const handleError = async (error) => {
-		if (error.response) {
-			if (error.response.status === 401) {
-				return 'Invalid credentials, please try again.';
-			}
-			return 'Server error, please try again later.';
-		} else if (error.request) {
-			return 'Network error, please check your connection.';
+	  if (error.response) {
+		if (error.response.status === 401) {
+			return 'Invalid credentials, please try again.';
 		}
-		return 'An expected error occured';
+		return 'Server error, please try again later.';
+	  } else if (error.request) {
+		return 'Network error, please check your connection.';
+	  }
+	  return 'An expected error occured';
 	};
 	
 	const userLogin = async (accountName, pw) => {
 		try {
 			const response = await loginValidate(accountName, pw);
 			const userData = response.data;
-			setLoggedIn(true);
+			
+			localStorage.setItem('token', userData.token);
+			localStorage.setItem('user', JSON.stringify(userData.user));
+			
+			
 			setUser(userData[0]);
+			setLoggedIn(true);
+			setAdmin(null);
+			setAdminLoggedIn(false);
+
 			setError(null);
 			return response;
 			// Store JWT token in local storage
@@ -43,16 +61,23 @@ const AuthProvider = ({ children }) => {
 	};
 	
 	const logout = () => {
+		localStorage.removeItem('token');
+		localStorage.removeItem('user');
+		localStorage.removeItem('admin');
+		
 		setUser(null);
 		setLoggedIn(false);
 		setError(null);
 		setAdminLoggedIn(false);
+		setAdmin(null);
 	};
 	
 	const adminLogin = async (accountName, pw) => {
 		try {
 			const response = await adminLoginValidate(accountName, pw);
 			const adminData = response.data;
+			localStorage.setItem('token', adminData.token);
+			localStorage.setItem('admin', JSON.stringify(adminData.admin));
 			setAdmin(adminData[0]);
 			setAdminLoggedIn(true);
 			setError(null);
